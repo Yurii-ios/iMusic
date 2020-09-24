a62cbb9635581d284a75715f7ebc39dbcc3f557a
Yurii <Yurii>
2020-09-24 20:26:43 +0200

    Create SearchViewController

---
+++ iMusic/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  iMusic
+//
+//  Created by Yurii Sameliuk on 24/09/2020.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
---
+++ iMusic/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  iMusic
+//
+//  Created by Yurii Sameliuk on 24/09/2020.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
