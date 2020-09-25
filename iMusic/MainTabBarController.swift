6c4c9bcbc4ffeeeec8c9d3cfac2c98c7f45ae369
Yurii <Yurii>
2020-09-25 18:59:55 +0200

    Add searchController,  trucks array

--- iMusic/MainTabBarController.swift
+++ iMusic/MainTabBarController.swift
@@ -12,7 +12,10 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        
+        // meniaem cvet  knopok tabBara
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
        
         // dobawliaem kontrolleru kotorue mu chotim videt w tab bare
         viewControllers = [generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"), generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library")]
--- iMusic/SearchViewController.swift
+++ iMusic/SearchViewController.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchViewController: UITableViewController {
+    
+    // chranit wse treki kotorue imeem
+    let tracks = [TrackModel(trackName: "xxx", artistName: "XXX"), TrackModel(trackName: "eeee", artistName: "EEEE")]
+    
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // wozwr kol trekow
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = " \(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        cell.imageView?.image = #imageLiteral(resourceName: "Image")
+        return cell
     }
 }
