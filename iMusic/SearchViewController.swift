d767819cb6ce9ffbf9809e01709460971dd81b8f
Yurii <Yurii>
2020-09-27 15:46:48 +0200

    refactoring parameters method AlamofireRequest

--- iMusic/SearchViewController.swift
+++ iMusic/SearchViewController.swift
@@ -16,67 +16,74 @@ struct TrackModel {
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-       // print(searchText)
-       
-        let url = "https://itunes.apple.com/search?term=\(searchText)"
-        Alamofire.request(url).responseData { (dataResponse) in
-            if let error = dataResponse.error {
-                print(error.localizedDescription)
-                return
-            }
-            
-            guard let data = dataResponse.data else { return }
-            // decodiryem dannue
-            let decoder = JSONDecoder()
-            do {
-                let objects = try decoder.decode(SearchResponse.self, from: data)
-                print(objects)
-            } catch let error {
-                print(error.localizedDescription)
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            let url = "https://itunes.apple.com/search"
+            let parametrs = ["term":"\(searchBar)","limit":"15"]
+            Alamofire.request(url, method: HTTPMethod.get, parameters: parametrs, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+                if let error = dataResponse.error {
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let data = dataResponse.data else { return }
+                // decodiryem dannue
+                let decoder = JSONDecoder()
+                do {
+                    let objects = try decoder.decode(SearchResponse.self, from: data)
+                    print(objects)
+                    // zapolniaem masiw poly4ennumi dannumi
+                    self.tracks = objects.results
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                } catch let error {
+                    print("error:\(error.localizedDescription)")
+                    
+                }
+                let someString = String(data: data, encoding: String.Encoding.utf8)
                 
             }
             
-            let someString = String(data: data, encoding: String.Encoding.utf8)
-            print("This is my string: \(someString ?? "Empty string")")
+        })
         }
-    
-    }
 }
-
-class SearchViewController: UITableViewController {
-    
-    // chranit wse treki kotorue imeem
-    let tracks = [TrackModel(trackName: "xxx", artistName: "XXX"), TrackModel(trackName: "eeee", artistName: "EEEE")]
-    
-    let searchController = UISearchController(searchResultsController: nil)
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    class SearchViewController: UITableViewController {
         
-        view.backgroundColor = .white
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        private var timer: Timer?
         
-        setupSearchBar()
+        // chranit wse treki kotorue imeem
+        private var tracks = [Track]()
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            view.backgroundColor = .white
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+            
+            setupSearchBar()
+        }
+        
+        private func setupSearchBar() {
+            navigationItem.searchController = searchController
+            navigationItem.hidesSearchBarWhenScrolling = false
+            searchController.searchBar.delegate = self
+        }
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // wozwr kol trekow
+            return tracks.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+            let track = tracks[indexPath.row]
+            cell.textLabel?.text = " \(track.trackName)\n\(track.artistName)"
+            cell.textLabel?.numberOfLines = 2
+            cell.imageView?.image = #imageLiteral(resourceName: "Image")
+            return cell
+        }
     }
-    
-    private func setupSearchBar() {
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
-        searchController.searchBar.delegate = self
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // wozwr kol trekow
-        return tracks.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        let track = tracks[indexPath.row]
-        cell.textLabel?.text = " \(track.trackName)\n\(track.artistName)"
-        cell.textLabel?.numberOfLines = 2
-        cell.imageView?.image = #imageLiteral(resourceName: "Image")
-        return cell
-    }
-}
 
