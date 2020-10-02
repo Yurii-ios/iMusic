d6457682d61ee3cca043e5aef55fa753a932edf3
Yurii <Yurii>
2020-10-02 11:34:27 +0200

    work on TrackDetailView.xib file

--- iMusic/Search/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView.swift
@@ -8,8 +8,33 @@
 import UIKit
 
 class TrackDetailView: UIView {
+    @IBOutlet var trackImageView: UIImageView!
+    @IBOutlet var currentTimeSlider: UISlider!
+    @IBOutlet var currentTimeLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var trackTitleLabel: UILabel!
+    @IBOutlet var authorTitleLabel: UILabel!
+    @IBOutlet var playPayseButton: UIButton!
+    @IBOutlet var volumeSlider: UISlider!
     
     override class func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+        
+    }
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+      // udaliaem view
+        self.removeFromSuperview()
+    }
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
     }
 }
