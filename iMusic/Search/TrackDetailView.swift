6506892afe79851ba97aae618e74be30bb806187
Yurii <Yurii>
2020-10-02 19:59:42 +0200

    add AVPlayer

--- iMusic/Search/TrackDetailView.swift
+++ iMusic/Search/TrackDetailView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SDWebImage
+import AVKit
 
 class TrackDetailView: UIView {
     @IBOutlet var trackImageView: UIImageView!
@@ -18,6 +19,13 @@ class TrackDetailView: UIView {
     @IBOutlet var playPayseButton: UIButton!
     @IBOutlet var volumeSlider: UISlider!
     
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        //snizaem zaderzky wuzowa avPlayer pri ego wuzowe
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
     override class func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -26,14 +34,22 @@ class TrackDetailView: UIView {
     func set(viewModel: SearchViewModel.Cell) {
         trackTitleLabel.text = viewModel.trackName
         authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
+        
         // izmeniaem razmer izobrazenija
         let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
         
         // priwodim string k URL
         guard let url = URL(string: string600 ?? "") else { return }
-        
         // zagryzaem image
         trackImageView.sd_setImage(with: url, completed: nil)
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        guard let url = URL(string: previewUrl ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
     
     @IBAction func handleCurrentTimeSlider(_ sender: Any) {
@@ -51,5 +67,12 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPayseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPayseButton.setImage(#imageLiteral(resourceName: "play"), for: UIControl.State.normal)
+        }
     }
 }
