f3d83435b85481795999fce1cfc56bc7c0fc93df
Yurii <Yurii>
2020-10-11 15:31:24 +0200

    hide addTrackOutlet button when track was preserved

--- iMusic/Search/TrackCell.swift
+++ iMusic/Search/TrackCell.swift
@@ -28,6 +28,8 @@ class TrackCell: UITableViewCell {
     
     @IBOutlet var trackImageView: UIImageView!
     
+    @IBOutlet var addTrackOutlet: UIButton!
+    
     // danuj metod wuzuwaetsia tolko w tom sly4ae kogda cell skonfigyirowan 4erez xib
     override class func awakeFromNib() {
         super.awakeFromNib()
@@ -42,9 +44,23 @@ class TrackCell: UITableViewCell {
     
     var cell: SearchViewModel.Cell?
     
+    // srabatuwaet kogda mu podgryzaem nowyjy ja4ejky
     func set(viewModel: SearchViewModel.Cell) {
         
         self.cell = viewModel
+        // proweriaem dobawlen li treck w userDefaults
+        let savedTracks = UserDefaults.standard.savedTracks()
+        // proweriaem sowpadajyt li dannue w savedTracks masiwe s dannumi w viewModel
+        let hasFavourite = savedTracks.firstIndex {
+            $0.trackName == self.cell?.trackName && $0.artistName == self.cell?.artistName
+        } != nil
+        // esli est w masiwe - pria4em knopky addTrackAction
+        if hasFavourite {
+            addTrackOutlet.isHidden = true
+        } else {
+            addTrackOutlet.isHidden = false
+        }
+        
         trackNameLabel.text = viewModel.trackName
         artistNameLabel.text = viewModel.artistName
         collectionNameLabel.text = viewModel.collectionName
@@ -57,23 +73,14 @@ class TrackCell: UITableViewCell {
         let defaults = UserDefaults.standard
         // soderzut masiw so spiskom trekov
         guard let cell = cell else { return }
+        addTrackOutlet.isHidden = true
+        
         var listOfTracks = defaults.savedTracks()
         listOfTracks.append(cell)
         
         // sochraniaem dannue kotorue chraniatsia w cell
         if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: listOfTracks , requiringSecureCoding: false) {
             defaults.setValue(savedData, forKey: UserDefaults.favouriteTrackKey)
-        }
-    }
-    @IBAction func showInfoAction(_ sender: Any) {
-        let defaults = UserDefaults.standard
-        if let savedTracks = defaults.object(forKey: UserDefaults.favouriteTrackKey) as? Data {
-            // razarchiwiryem dannue
-            if let decodedTracks = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedTracks) as? [SearchViewModel.Cell] {
-                decodedTracks.map { (track)  in
-                    print(track.trackName)
-                }
-            }
         }
     }
 }
